fba67199ef8eeb41a92692dec3542fe42740d912
Aliesky Pérez <aliesky.perez@gmail.com>
2021-03-20 17:21:08 -0400

    fixed developed envairoments

--- src/global/config.js
+++ src/global/config.js
@@ -1,5 +1,3 @@
-import { string } from "prop-types";
-
 /** @constant {string} API_BASE_URL*/
-const url= process.env.API_BASE_URL? process.env.API_BASE_URL : "https://arane-crm-vl17-be-stg.herokuapp.com"
-export const API_BASE_URL = process.env.API_BASE_URL;
+//const url= process.env.API_BASE_URL? process.env.API_BASE_URL : "https://arane-crm-vl17-be-stg.herokuapp.com"
+export const API_BASE_URL = process.env.REACT_APP_API_URL;
