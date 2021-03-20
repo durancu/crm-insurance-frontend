1f1ef70658b40e459db4c1cb3310b6f8db1fc962
Liuver Duran <liuver@gmail.com>
2021-03-20 01:46:43 -0500

    embed heroku because missing environment variables

--- src/global/config.js
+++ src/global/config.js
@@ -1,2 +1,5 @@
+import { string } from "prop-types";
+
 /** @constant {string} API_BASE_URL*/
+const url= process.env.API_BASE_URL? process.env.API_BASE_URL : "https://arane-crm-vl17-be-stg.herokuapp.com"
 export const API_BASE_URL = process.env.API_BASE_URL;
